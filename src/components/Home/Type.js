2b25063484f9561b78d715387e12ee8cafed410b
pulkitkumar99050-code <pulkitkumar99050@gmail.com>
2025-12-31 18:49:26 +0530

    Update Type.js

--- src/components/Home/Type.js
+++ src/components/Home/Type.js
@@ -8,7 +8,7 @@ function Type() {
         strings: [
           "Space lover",
           "Stargazer",
-          "A Simple Loving Creature",
+          "A Simple Creature",
           "Finding my मंज़िल ",
         ],
         autoStart: true,
